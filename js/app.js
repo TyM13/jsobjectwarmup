e1f630b1b4e3300ff80c6385b9dbe8168a37a461
Your Name <you@example.com>
2022-07-12 16:31:59 -0600

    warmup week 10

---
+++ js/app.js
@@ -0,0 +1,16 @@
+let my_city = [
+    {
+        name: `calgary`,
+        population: 1611000,
+        is_capital: false,
+    }
+];
+
+let counter = 0;
+if (my_city[counter][`is_capital`] = true) {
+console.log(`city: ${my_city[counter][`name`]}`);
+console.log(`population: ${my_city[counter][`population`]}`);
+
+} else {
+    console.log(`not importan enough`)
+}
